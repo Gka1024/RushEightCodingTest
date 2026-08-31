da12f4fbf7a08792000f9fc393341f86abbe3d94
Gka1024 <ththtlddkswn@gmail.com>
2026-08-31 18:15:16 +0900

    [ADD] Station Connect Algorithm

--- SubwayList.cs
+++ SubwayList.cs
@@ -4,7 +4,28 @@ namespace Subway
     {
         public static void Initialize()
         {
-            Console.WriteLine("Init");
+            List<Station> stationsLine1 = new();
+            List<Station> stationsLine2 = new();
+            List<Station> stationsLine3 = new();
+            List<Station> stationsLine4 = new();
+            List<Station> stationsLine5 = new();
+            List<Station> stationsLine6 = new();
+
+            RegisterStation4();
+        }
+
+        private static void RegisterStation4()
+        {
+            Station ichon = new Station("이촌", 4);
+            Station sinyongsan = new Station("신용산", 4);
+
+            Connect(ichon, sinyongsan, 100);
+        }
+
+        private static void Connect(Station A, Station B, int time)
+        {
+            A.edges.Add(new Edge(B, time));
+            B.edges.Add(new Edge(A, time));
         }
     }
 
@@ -12,11 +33,24 @@ namespace Subway
     {
         public string name;
         public int line;
+        public List<Edge> edges;
 
         public Station(string name, int line)
         {
             this.name = name;
             this.line = line;
+        }
+    }
+
+    public class Edge
+    {
+        public Station destination;
+        public int time;
+
+        public Edge(Station destination, int time)
+        {
+            this.destination = destination;
+            this.time = time;
         }
     }
 
