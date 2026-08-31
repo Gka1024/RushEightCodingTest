d1f682b8823645dc3e1e494ef8195992dc9ab914
Gka1024 <ththtlddkswn@gmail.com>
2026-09-01 01:11:19 +0900

    [ADD] Dijkstra Algorithm (wip)

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using Subway;
 
 class Program
 {
@@ -9,6 +11,56 @@ class Program
         string start = Console.ReadLine()!;
         string end = Console.ReadLine()!;
 
+        if (start == end)
+        {
+            Console.WriteLine("같은거 입력하지 마세요");
+            return;
+        }
+
         Console.WriteLine(start + end);
     }
+
+
+    // 참고 : https://hsh12345.tistory.com/221
+    public List<Station> Dijkstra(Station start, Station dest)
+    {
+        int nodesCount = Subway.SubwayList.GetAllStationCount();
+        Dictionary<Station, bool> isVisited = new();
+        Dictionary<Station, int> totalDistance = new();
+        Dictionary<Station, Station?> parent = new();
+
+        foreach(Station station in SubwayList.GetAllStation())
+        {
+            isVisited.Add(station, false);
+            totalDistance.Add(station, int.MaxValue);
+        }
+
+        parent.Add(start, null);
+
+        while(true)
+        {
+            Station? now = null;
+            int closest = int.MaxValue;
+
+            for(int i = 0; i < nodesCount; i++)
+            {
+                if(now == null) break;
+
+                if(isVisited.TryGetValue(now, out bool value) && value) continue;
+                
+                if(totalDistance.TryGetValue(now, out int dist) && dist == int.MaxValue) continue;
+
+                if(dist < closest)
+                {
+                    closest = dist;
+                    // ???
+                }
+
+            }
+        }
+
+        
+    }
+
+
 }
--- SubwayList.cs
+++ SubwayList.cs
@@ -3,10 +3,11 @@ namespace Subway
     public static class SubwayList
     {
         public static Dictionary<int, List<Station>> stationsByLine = new();
-        
+
+
         public static void Initialize()
         {
-            for(int i = 1; i <= 6; i++)
+            for (int i = 1; i <= 6; i++)
             {
                 stationsByLine[i] = new();
             }
@@ -14,39 +15,89 @@ namespace Subway
             RegisterStation4(stationsByLine[4]);
         }
 
-        private static void RegisterStation4(List<Station> line)
-        {
-            // ==== 역 생성
-            Station ichon = new Station("이촌", 4);
-            Station sinyongsan = new Station("신용산", 4);
-
-
-            // ==== 역 연결
-            Connect(ichon, sinyongsan, 100);
-
-
-            // ==== 역 추가
-            line.Add(ichon);
-            line.Add(sinyongsan);
-        }
-
         private static void Connect(Station A, Station B, int time)
         {
             A.edges.Add(new Edge(B, time));
             B.edges.Add(new Edge(A, time));
         }
+
+        public static int GetAllStationCount()
+        {
+            int count = 0;
+            for (int i = 1; i <= 6; i++)
+            {
+                count += stationsByLine[i].Count;
+            }
+            return count;
+        }
+
+        public static List<Station> GetAllStation()
+        {
+            List<Station> stations = new();
+            for (int i = 1; i <= 6; i++)
+            {
+                stations.AddRange(stationsByLine[i]);
+            }
+            return stations;
+        }
+
+
+        private static void RegisterStation4(List<Station> line)
+        {
+            // ==== 역 생성
+            Station ichon = new Station("이촌", 4);
+            Station sinyongsan = new Station("신용산", 4);
+            Station samgakji = new Station("삼각지", 4);
+            Station sukdaeipgue = new Station("숙대입구", 4);
+            Station seoulstation = new Station("서울역", 4);
+            Station hoihyun = new Station("회현", 4);
+            Station myeongdong = new Station("명동", 4);
+            Station chungmuro = new Station("충무로", 4);
+            Station DHCP = new Station("동대문역사문화공원", 4);
+            Station dongdaemun = new Station("동대문", 4);
+            Station hyehwa = new Station("혜화", 4);
+
+
+            // ==== 역 연결
+            Connect(ichon, sinyongsan, 100);
+            Connect(sinyongsan, samgakji, 90);
+            Connect(samgakji, sukdaeipgue, 100);
+            Connect(sukdaeipgue, seoulstation, 100);
+            Connect(seoulstation, hoihyun, 90);
+            Connect(hoihyun, myeongdong, 90);
+            Connect(myeongdong, chungmuro, 80);
+            Connect(chungmuro, DHCP, 100);
+            Connect(DHCP, dongdaemun, 90);
+            Connect(dongdaemun, hyehwa, 90);
+
+
+            // ==== 역 추가
+            line.Add(ichon);
+            line.Add(sinyongsan);
+            line.Add(samgakji);
+            line.Add(sukdaeipgue);
+            line.Add(seoulstation);
+            line.Add(hoihyun);
+            line.Add(myeongdong);
+            line.Add(chungmuro);
+            line.Add(DHCP);
+            line.Add(dongdaemun);
+            line.Add(hyehwa);
+        }
+
+
     }
 
     public class Station
     {
-        public string name;
-        public int line;
+        public string Name { get; private set; }
+        public int Line { get; private set; }
         public List<Edge> edges = new();
 
         public Station(string name, int line)
         {
-            this.name = name;
-            this.line = line;
+            Name = name;
+            Line = line;
         }
     }
 
